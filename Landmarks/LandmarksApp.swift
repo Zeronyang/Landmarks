7578e0586849d0c1597b2381f02ad23205968ea4
Zerony <63838863+Zeronyang@users.noreply.github.com>
2020-10-15 23:24:30 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 杨子丰 on 2020/10/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
